bac9d6a5a92350a38b712047cf68582133331b8f
Evan Saracay <evansaracay@gmail.com>
2020-07-14 19:48:03 +0000

    Add a completed query algorithm for week-5

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,10 +15,79 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.lang.Boolean;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-    //idea use recursion over all attendees and see if you can find a start time that works for everyone
+    List<TimeRange> verifiedTimes = new ArrayList<TimeRange>();
+    ArrayList<Event> listEvents = new ArrayList<>(events);
+    HashSet<String> meetingGroup = new HashSet<>(request.getAttendees());
+    ArrayList<Event> sortedListEvents = mySort(listEvents, meetingGroup);
+
+    int slotStart = 0;
+    TimeRange previousSlot = null;
+    for(Event event: sortedListEvents) {
+        TimeRange usedSlot = event.getWhen();
+        // checking for overlapping events
+        if(previousSlot == null || !usedSlot.overlaps(previousSlot)){
+          verifiedTimes.add(TimeRange.fromStartEnd(slotStart, usedSlot.start(), false));
+          slotStart = usedSlot.end();
+          previousSlot = usedSlot;
+        }else if(previousSlot != null && previousSlot.contains(usedSlot)) {
+            //checking for nested events
+            slotStart = previousSlot.end();
+        } else {
+            slotStart = usedSlot.end();
+            previousSlot = usedSlot;
+        }
+    }
+     
+    // if the last event is not at the end of the day then we want to include
+    // the timerange from end of last event to the end of the day. 
+    if (slotStart != 60 * 24){
+      verifiedTimes.add(TimeRange.fromStartEnd(slotStart, 60 * 24, false));
+    }
+
+    // Get rid of short time slots
+    for(int i = 0; i < verifiedTimes.size(); i++ ) {
+        if (verifiedTimes.get(i).duration() < request.getDuration()) {
+          verifiedTimes.remove(i);
+          i--;
+        }
+    }
+
+    
+
+    return verifiedTimes;
   }
-}
+
+  public ArrayList<Event> mySort(ArrayList<Event> list, HashSet<String> meetingGroup) {
+      ArrayList<Event> newList = new ArrayList<>();
+      while(list.size() > 0) {
+          int indexMin = 0;
+          for(int i = 1; i < list.size(); i++) {
+            int newTime = list.get(i).getWhen().start(); 
+            if(list.get(indexMin).getWhen().start() > newTime) {
+                indexMin = i;
+            }
+          }
+          Event closestEvent = list.get(indexMin);
+          list.remove(indexMin);
+          // Checks to make sure that a meetingGroup member is actually in this event
+          HashSet<String> attendees = new HashSet<>(closestEvent.getAttendees());
+          attendees.retainAll(meetingGroup);
+          if(attendees.size() <= 0) {
+              continue;
+          } else {
+              newList.add(closestEvent);
+          }
+      }
+      return newList;
+    }
+
+  }
